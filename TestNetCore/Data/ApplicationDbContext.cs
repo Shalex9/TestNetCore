65ba6b15e545a523f04f6e70f0cca735b2ead910
Yuriy <yuriy_zaletsiy@yahoo.com>
2018-12-07 14:43:24 +0200

    delete RusWord

--- TestNetCore/Data/ApplicationDbContext.cs
+++ TestNetCore/Data/ApplicationDbContext.cs
@@ -27,7 +27,6 @@ namespace TestNetCore.Data
         public DbSet<HotelReservation> HotelReservations { get; set; }
         public DbSet<HotelInformation> HotelInformations { get; set; }
         public DbSet<SettingsPageGallery> SettingsGalleryPages { get; set; }
-        //public DbSet<RussianWord> RussianWords { get; set; }
         public DbSet<RussianDictionary> RussianDictionaries { get; set; }
         public DbSet<ForbiddenWord> ForbiddenWords { get; set; }
         public DbSet<ForbiddenWordUser> ForbiddenWordUsers { get; set; }
