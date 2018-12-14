7291f9a39d9b45b2b1468c076eb288123e614221
Shalex9 <shalex9@gmail.com>
2018-12-14 11:31:30 +0200

    forbidden words

--- TestNetCore/Controllers/Widgets/MessagesController.cs
+++ TestNetCore/Controllers/Widgets/MessagesController.cs
@@ -13,6 +13,7 @@ using System.Net.Mail;
 using Newtonsoft.Json;
 using TestNetCore.Data.TableData;
 using TestNetCore.Models.Widgets.Messages;
+using System.Text.RegularExpressions;
 
 namespace TestNetCore.Controllers
 {
@@ -20,8 +21,7 @@ namespace TestNetCore.Controllers
     {
         public MessagesController(IHttpContextAccessor httpContextAccessor, ApplicationDbContext dbContext)
             : base(httpContextAccessor, dbContext)
-        {
-        }
+        { }
 
 
         [HttpGet]
@@ -34,6 +34,7 @@ namespace TestNetCore.Controllers
             return View("~/Views/Widgets/Messages.cshtml", viewModel);
         }
 
+
         public MessagesViewModel ModelForMessages(MessagesViewModel viewModel)
         {
             // Проверяю или существует директория для файлов для этого юзера, если нет - создаю
@@ -43,39 +44,92 @@ namespace TestNetCore.Controllers
                 Directory.CreateDirectory(directoryFilesUser);
             }
 
-
+            viewModel.SendTo = UserEmail;
             viewModel.UserName = CurrentUserName;
             viewModel.AvatarPath = CurrentAvatarPath;
             return viewModel;
         }
 
+
         [HttpPost]
-        [Route("hotel")]
+        [Route("messages")]
         public IActionResult Messages(List<IFormFile> files, MessagesViewModel viewModel)
         {
-
-            SendEMail(UserEmail, viewModel.NameFrom, viewModel.TitleMessage, viewModel.TextMessage, viewModel.AttachFile, UserID);
-
-            viewModel = ModelForMessages(viewModel);
-
-            return View("~/Views/Widgets/Hotel.cshtml", viewModel);
-        }
-
-        public MessagesViewModel UploadFile(dynamic file, MessagesViewModel viewModel)
-        {
-            var fileName = Path.GetFileName(file.FileName);
-            var path = Path.Combine("wwwroot/usersfiles/" + UserID + "/imgForEmail/", fileName);
-            var size = (file.Length / 1024) + "КБ";
-
-            if (size <= 15000)
+            if (viewModel.PostType == "uploadFiles")
             {
-                using (var stream = new FileStream(path, FileMode.Create))
+                foreach (var file in files)
                 {
-                    file.CopyTo(stream);
+                    if (file != null && file.Length > 0)
+                    {
+                        viewModel.AttachFile = UploadFile(file);
+                    }
                 }
             }
 
-            return viewModel;
+            if (viewModel.PostType == "sendEmail")
+            {
+                // Create List Forbidden Words (Standart Forbidden Words + User )
+                var forbiddenWordsUser = _dbContext.ForbiddenWordUsers.Where(u => u.UserId == UserID);
+                var forbiddenWordStandart = _dbContext.ForbiddenWords.Where(a => a.Word != null);
+
+                // for Change forbidden words to ***
+                if (viewModel.TextMessage != null)
+                {
+                    viewModel.TextMessage = RemoveWords(viewModel.TextMessage, forbiddenWordsUser, forbiddenWordStandart, viewModel);
+                }
+                SendEMail(UserEmail, viewModel.NameFrom, viewModel.TitleMessage, viewModel.TextMessage, viewModel.AttachFile, UserID);
+            }
+
+            if (viewModel.PostType == "saveSettings")
+            {
+                var forbiddenWord = _dbContext.ForbiddenWordUsers.Where(u => u.UserId == UserID);
+                var forbiddenWordStandart = _dbContext.ForbiddenWords.Where(a => a.Word != null);
+
+                var stringForbiddenWords = viewModel.StringForbiddenWordsUser;
+                if (stringForbiddenWords != null)
+                {
+                    var listForbiddenWords = stringForbiddenWords.Trim().Split(new[] { " ", "," }, StringSplitOptions.RemoveEmptyEntries);
+
+                    // удаляю из БД все запрещенные слова юзера
+                    var wordsUser = _dbContext.ForbiddenWordUsers.Where(a => a.UserId == UserID);
+                    foreach (var del in wordsUser)
+                    {
+                        _dbContext.ForbiddenWordUsers.Remove(del);
+                    }
+                    _dbContext.SaveChanges();
+
+                    if (listForbiddenWords != null)
+                    {
+                        foreach (string word in listForbiddenWords)
+                        {
+                            ForbiddenWordUser w = new ForbiddenWordUser();
+                            // на всякий случай сравниваю массив новых слов юзера со словами из таблицы, если такого слова нет - добавляю в таблицу
+                            if (_dbContext.ForbiddenWordUsers.Where(a => a.Word == word).Count() == 0)
+                            {
+                                w.UserId = UserID;
+                                w.Word = word.ToLower();
+                                viewModel.ForbiddenWordsUser.Add(w);
+                                _dbContext.ForbiddenWordUsers.Add(w);
+                            }
+                        }
+                        _dbContext.SaveChanges();
+                    }
+                }
+                else
+                {
+                    // просто удаляю из БД все запрещенные слова юзера
+                    var wordsUser = _dbContext.ForbiddenWordUsers.Where(a => a.UserId == UserID);
+                    foreach (var del in wordsUser)
+                    {
+                        _dbContext.ForbiddenWordUsers.Remove(del);
+                    }
+                    _dbContext.SaveChanges();
+                }
+            }
+
+            viewModel = ModelForMessages(viewModel);
+
+            return View("~/Views/Widgets/Messages.cshtml", viewModel);
         }
 
 
@@ -106,5 +160,52 @@ namespace TestNetCore.Controllers
             }
         }
 
+
+        public string UploadFile(dynamic file)
+        {
+            var fileName = Path.GetFileName(file.FileName);
+            var path = Path.Combine("wwwroot/usersfiles/" + UserID + "/imgForEmail/", fileName);
+            var size = (file.Length / 1024) + "КБ";
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+
+            return fileName;
+        }
+
+
+        public static string RemoveWords(string textForReplace,
+                                    IQueryable<ForbiddenWordUser> forbiddenWordUser,
+                                    IQueryable<ForbiddenWord> forbiddenWordStandart,
+                                    MessagesViewModel viewModel)
+        {
+            foreach (var word in forbiddenWordUser)
+            {
+                var forbidden = new ForbiddenWordUser();
+                forbidden.Word = word.Word;
+                viewModel.ForbiddenWordsUser.Add(forbidden);
+                viewModel.AllForbiddenWordsUser.Add(forbidden);
+            }
+            foreach (var word in forbiddenWordStandart)
+            {
+                var forbidden = new ForbiddenWordUser();
+                forbidden.Word = word.Word;
+                viewModel.ForbiddenWordsStandart.Add(forbidden);
+                viewModel.AllForbiddenWordsUser.Add(forbidden);
+            }
+
+            List<string> arrayForbidden = new List<string>();
+            foreach (var w in viewModel.AllForbiddenWordsUser)
+            {
+                arrayForbidden.Add(w.Word.ToLower());
+            }
+
+            string pattern = "\\b" + string.Join("\\b|\\b", arrayForbidden) + "\\b";
+            string replace = "***";
+            textForReplace = Regex.Replace(textForReplace, pattern, replace, RegexOptions.IgnoreCase);
+            return textForReplace;
+        }
     }
 }
--- TestNetCore/Models/Widgets/Messages/MessagesViewModel.cs
+++ TestNetCore/Models/Widgets/Messages/MessagesViewModel.cs
@@ -15,5 +15,18 @@ namespace TestNetCore.Models.Widgets.Messages
         public string TextMessage { get; set; }
         public string AttachFile { get; set; }
         public string PostType { get; set; }
+
+        // Вся строка запрещенных слов которую разбираю по пробелу и делаю List
+        public string StringForbiddenWordsUser { get; set; }
+
+        public List<ForbiddenWordUser> ForbiddenWordsUser { get; set; }
+        public List<ForbiddenWordUser> ForbiddenWordsStandart { get; set; }
+        public List<ForbiddenWordUser> AllForbiddenWordsUser { get; set; }
+        public MessagesViewModel()
+        {
+            ForbiddenWordsUser = new List<ForbiddenWordUser>();
+            ForbiddenWordsStandart = new List<ForbiddenWordUser>();
+            AllForbiddenWordsUser = new List<ForbiddenWordUser>();
+        }
     }
 }
